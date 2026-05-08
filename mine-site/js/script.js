330a6972d1d9262f8681fbfb6dcb1ee3eb50d22a
sameuu01 <samuel.clsmorais@gmail.com>
2026-05-08 14:54:53 -0300

    Atualizando

--- mine-site/js/script.js
+++ mine-site/js/script.js
@@ -1,3 +1,14 @@
-for(let i=0; i <=5; i++) {
-console.log("O valor de i, vale: " + i)
+/*
+  - Escreva um loop (for) que exiba a frase abaixo
+   no console;
+  - Substitua o "X" pela informação correta;
+  - "X" deve começar em 1 na 1ª exibição da frase e em 
+  10 na última;
+  - Exiba a frase 10x no console.
+
+  "Esta é a Xª vez que esta frase é exibida no console."
+*/
+
+for (let x = 1; x <=10; x++) {
+    console.log(`Esta é a ${x}ª vez que esta frase é exibida no console.`)
 }
