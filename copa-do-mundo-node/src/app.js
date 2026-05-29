dfbba0662a9ad9f58626e438e79dd70d0cfe8746
sameuu01 <samuel.clsmorais@gmail.com>
2026-05-29 16:25:34 -0300

    feat: add js

--- copa-do-mundo-node/src/app.js
+++ copa-do-mundo-node/src/app.js
@@ -20,4 +20,45 @@ app.get('/selecoes', (req, res) => {
     })
 })
 
+// Buscando por ID
+app.get('/selecoes/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "select * from selecoes where id=?;";
+
+    conexao.query(sql, id, (erro, result) => {
+        res.json(result[0]);
+    })
+})
+
+// Criando POST para cadastrar
+app.post('/selecoes', (req, res) => {
+    const selecao = req.body;
+    const sql = "insert into selecoes set?;";
+
+    conexao.query(sql, selecao, () => {
+        res.json({ mensagem: "Cadastrado com sucesso!"});
+    })
+})
+
+//Deletando registro
+app.delete('/selecoes/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "delete from selecoes where id=?";
+
+    conexao.query(sql, id, () => {
+        res.json({ mensagem: "Deletado com sucesso"});
+    })
+})
+
+// Atualizando registro
+app.put('/selecoes/:id', (req, res) => {
+    const id = req.params.id;
+    const selecao = req.body;
+    const sql = "update selecoes set ? where id=?";
+
+    conexao.query(sql, [selecao, id], () => {
+        res.json({ mensagem: "Atualizando com sucesso!"});
+    });
+});
+
 export default app;
